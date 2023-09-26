830623e42e9871770a978029e406158e5d8d3d4a
Niklas Palm <niklas-palm@hotmail.com>
2023-09-26 11:49:14 +0200

    refactor to use setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="ai-cli",
-    version="1.0",
-    py_modules=["hackathon"],
-    install_requires=["Click", "boto3"],
-    entry_points="""
-        [console_scripts]
-        hack=hackathon:cli
-    """,
-)
+setup()
